d95e866410fbdb4fe42c7a06e22de2be43713c3f
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-15 11:35:20 -0700

    Adding Form/Form Handling to root modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,22 @@
 "use client"
-import { Modal } from "@/components/ui/modal";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 
 export default function SetupPage() {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if(!isOpen){
+      onOpen();
+    }
+  }, [ isOpen, onOpen])
+
   return (
-    <div>
-       <Modal title="Test" description="test desc" isOpen onClose={() => {}}>
-        Children
-       </Modal>
+    <div className="p-4">
+       Root Page
     </div>
   )
 }
