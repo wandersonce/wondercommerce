142f85df0a781d4523ff29f939fc9b38a582838a
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-17 12:43:04 -0700

    Creating Settings main page layout

---
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,37 @@
+import SettingsForm from "@/components/SettingsForm";
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
+interface SettingPageProps{
+  params:{
+    storeId: string;
+  }
+}
+
+export default async function page({params} : SettingPageProps) {
+  const {userId} = auth();
+
+  if(!userId){
+    redirect('/sign-in');
+  }
+
+  const store = await prismadb.store.findFirst({
+    where:{
+      id: params.storeId,
+      userId
+    }
+  })
+
+  if(!store){
+    redirect('/');
+  }
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm />
+      </div>
+    </div>
+  )
+}
---
+++ components/SettingsForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SettingsForm() {
+  return (
+    <div>
+      Settings form
+    </div>
+  )
+}
