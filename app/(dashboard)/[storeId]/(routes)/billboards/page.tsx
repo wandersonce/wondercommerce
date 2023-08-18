54a43d4400a573a7bc9509d6508900dac6197e7c
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-18 13:56:22 -0700

    Creating billboard form and component

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,12 @@
+import Client from "./components/Client";
+
+
+export default function Billboards() {
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Client />
+      </div>
+    </div>
+  )
+}
--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -15,6 +15,11 @@ export default function MainNav({className, ...props} : React.HtmlHTMLAttributes
       active: pathname === `/${params.storeId}`
     },
     {
+      href: `/${params.storeId}/billboards`,
+      label: 'Billboards',
+      active: pathname === `/${params.storeId}/billboards`
+    },
+    {
     href: `/${params.storeId}/settings`,
     label: 'Settings',
     active: pathname === `/${params.storeId}/settings`
