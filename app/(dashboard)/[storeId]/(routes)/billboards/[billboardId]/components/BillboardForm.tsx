49462d038cf87789bdae4f37dc2ed20bce9b5c6f
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-18 14:42:01 -0700

    Editing api route calls on Billboard form

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -58,12 +58,15 @@ export default function BillboardForm({initialData} : BillboardFormProps) {
   const onSubmit = async (data: SettingsFormValues) => {
     try {
       setLoading(true);
-
-      await axios.patch(`/api/stores/${params.storeId}`, data);
+      if(initialData){
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+      }else{
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      }
 
       router.refresh();
 
-      toast.success("Store Updated.");
+      toast.success(toastMessage);
       
     } catch (error) {
       toast.error("Something went wrong.")
@@ -77,16 +80,16 @@ export default function BillboardForm({initialData} : BillboardFormProps) {
     try {
       setLoading(true);
 
-      await axios.delete(`/api/stores/${params.storeId}`);
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
 
       router.refresh();
 
       router.push("/");
 
-      toast.success("Store Deleted");
+      toast.success("Billboard Deleted");
       
     } catch (error) {
-      toast.error("Make sure you removed all products and categories first.")
+      toast.error("Make sure you removed categories using this billboard first.")
     } finally{
       setLoading(false);
       setOpen(false);
