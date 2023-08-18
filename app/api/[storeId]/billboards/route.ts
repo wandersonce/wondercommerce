d7ec6b17f06207349aea0c1f846b5f3b3193ceab
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-18 15:04:02 -0700

    Adding GET billboard routes

--- app/api/[storeId]/billboards/route.ts
+++ app/api/[storeId]/billboards/route.ts
@@ -53,3 +53,25 @@ export async function POST(req: Request, {params} : {params : {storeId : string}
     return new NextResponse("Internal error", {status: 500})
   }
 }
+
+export async function GET(req: Request, {params} : {params : {storeId : string}}){
+  try {
+
+    if(!params.storeId){
+      return new NextResponse("Store ID is required.", {status: 401})
+    }
+
+    const billboards = await prismadb.billboard.findMany({
+      where:{
+        storeId: params.storeId
+      }
+    })
+
+    return NextResponse.json(billboards)
+
+
+  } catch (error) {
+    console.log("[BILLBOARDS_GET]", error)
+    return new NextResponse("Internal error", {status: 500})
+  }
+}
