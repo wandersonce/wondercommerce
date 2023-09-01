aa131031f50cc843e00ec1e46af4c6e13310293f
Wanderson Castro <wandersonce1@hotmail.com>
2023-09-01 12:35:25 -0700

    Adding card

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,17 +1,23 @@
+import Heading from "@/components/ui/Heading";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb"
 
 interface DashboardPageProps{
   params: {storeId: string}
 }
 const DashboardPage:React.FC<DashboardPageProps> = async({params}) => {
-  const store = await prismadb.store.findFirst({
-    where:{
-      id: params.storeId
-    }
-  })
+  
 
   return (
-    <div>Active Store: {store?.name}</div>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
